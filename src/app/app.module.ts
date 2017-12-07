ab8a92ff178f579692d2f48472887afa8712ffe9
SlimDogs <SlimDogs@gmail.com>
2017-12-07 20:04:29 +0000

    fix(URL Strategy): Enabled hash url strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ export function HttpLoaderFactory(http: HttpClient) {
         deps: [HttpClient]
       }
     }),
-    RouterModule.forRoot([]),
+    RouterModule.forRoot([], { useHash: true }),
     IntroPageModule,
     MainPageModule
   ],
